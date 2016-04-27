5fdfd48d5c894c370c4347a88aa64a8306dc9767
Carlos Julio <cae001@gmail.com>
2016-04-28 00:00:09 +0100

    Change to have only 5 rotors and one reflector

--- enigma/Main.java
+++ enigma/Main.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 /**
  * Enigma simulator.
- * 
+ *
  * @author
  */
 public final class Main {
@@ -19,16 +19,12 @@ public final class Main {
 	 * Exits normally if there are no errors in the input; otherwise with code
 	 * 1.
 	 */
-	static Reflector reflectorB;
-	static Reflector reflectorC;
+	static Reflector reflector;
 	static Rotor rotor1;
 	static Rotor rotor2;
 	static Rotor rotor3;
 	static Rotor rotor4;
 	static Rotor rotor5;
-	static Rotor rotor6;
-	static Rotor rotor7;
-	static Rotor rotor8;
 
 	public static void main(String[] unused) {
 		Machine M = new Machine();
@@ -64,18 +60,15 @@ public final class Main {
 		if (!s[0].equals("*")) {
 			return false;
 		}
-		if (!s[1].equals("B") && !s[1].equals("C")) {
+		if (!(Arrays.asList(rotorList).contains(s[1])
+				&& Arrays.asList(rotorList).contains(s[2]) && Arrays.asList(
+				rotorList).contains(s[3]))) {
 			return false;
 		}
-		if (!(Arrays.asList(rotorList).contains(s[2])
-				&& Arrays.asList(rotorList).contains(s[3]) && Arrays.asList(
-				rotorList).contains(s[4]))) {
+		if (s[1].equals(s[2]) || s[1].equals(s[3]) || s[2].equals(s[3])) {
 			return false;
 		}
-		if (s[2].equals(s[3]) || s[2].equals(s[4]) || s[3].equals(s[4])) {
-			return false;
-		}
-		char[] init = s[5].toCharArray();
+		char[] init = s[4].toCharArray();
 		if (!isUpperLetter(init[0]) || !isUpperLetter(init[1])
 				|| !isUpperLetter(init[2]) || !isUpperLetter(init[3])) {
 			return false;
@@ -96,30 +89,20 @@ public final class Main {
 	 */
 	static void configure(Machine M, String config) {
 		String[] cArray = config.split(" ");
-		M.setRotors(getReflectorFromString(cArray[1]),
-				getRotorFromString(cArray[2]), getRotorFromString(cArray[3]),
-				getRotorFromString(cArray[4]));
-		M.setPositions(cArray[5]);
+		M.setRotors(getReflector(),
+				getRotorFromString(cArray[1]), getRotorFromString(cArray[2]),
+				getRotorFromString(cArray[3]));
+		M.setPositions(cArray[4]);
 
 	}
 
-	static Reflector getReflectorFromString(String s) {
-		Reflector r;
-		if (s.equals("B")) {
-			r = reflectorB;
-		}
-		else if (s.equals("C")) {
-			r = reflectorC;
-		}
-		else{
-			return null;
-		}
-		return r;
+	static Reflector getReflector() { //FromString(String s) {
+		return reflector;
 	}
 
 	/**
 	 * returns the numbered rotor from the roman numerals.
-	 * 
+	 *
 	 * @returns the Rotor
 	 * @param s
 	 *            roman numeral string
@@ -140,15 +123,6 @@ public final class Main {
 		}
 		else if (s.equals("V")) {
 			r = rotor5;
-		}
-		else if (s.equals("VI")) {
-			r = rotor6;
-		}
-		else if (s.equals("VII")) {
-			r = rotor7;
-		}
-		else if (s.equals("VIII")) {
-			r = rotor8;
 		}
 		else{
 			return null;
@@ -207,19 +181,8 @@ public final class Main {
 				"E S O V P Z J A Y Q U I R H X L N F T G K D C M W B", "J");
 		rotor5 = Rotor.rotor(
 				"V Z B R G I T Y U P S D N H L X A W M J Q O F E C K", "Z");
-		rotor6 = Rotor.rotor(
-				"J P G V O U M F Y Q B E N H Z R D K A S X L I C T W",
-				"Z and M");
-		rotor7 = Rotor.rotor(
-				"N Z J H G R C X M Y S W B O U F A I V L P E K Q D T",
-				"Z and M");
-		rotor8 = Rotor.rotor(
-				"F K Q H T L X O C B J S P D Z R A M E W N I U Y G V",
-				"Z and M");
-		reflectorB = Reflector
+		reflector = Reflector
 				.make("Y R U H Q S L D P X N G O K M I E B F Z C W V J A T");
-		reflectorC = Reflector
-				.make("F V P J I A O Y E D R Z X W G C T K U Q S B N M H L");
 	}
 
 }
